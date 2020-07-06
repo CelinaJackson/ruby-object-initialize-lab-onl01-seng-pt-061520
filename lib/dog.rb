9712c0277fe9a593756125a7d4bc52d1d30e39bf
Celina Jackson <celinanjackson@gmail.com>
2020-07-06 23:19:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,6 @@
 class Dog
-  def name = (dogs_name)
-    this_dogs_name = dogs_name
-  end
-  def name
-    this_dogs_name
-  end
-end
+  def initialize(name)
 
 lassie = Dog.new
+lassie.name = "Lassie"
+lassie.name
