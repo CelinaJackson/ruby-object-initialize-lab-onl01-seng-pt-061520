f71b65f5a712a58fa03ae375a637d862d71510dd
Celina Jackson <celinanjackson@gmail.com>
2020-07-06 23:24:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,17 @@
 class Dog
-  def initialize(name)
+  def initialize(name, breed = "Mutt")
     @name = name
+    @breed = breed
   end
-  def name=(name)
-    @name=name
+  def name=(name, breed = "Mutt")
+    @name = name
+    @breed = breed
   end
   def name
     @name
+  end
+  def breed 
+    @breed
   end
 end
 
